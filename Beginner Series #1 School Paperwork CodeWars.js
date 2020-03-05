d3e01a7d19f734ab577d04a37cb3c49e5bf7256a
Nazarenkopavel <50582798+Nazarenkopavel@users.noreply.github.com>
2020-03-05 20:27:30 +0200

    Create Beginner Series #1 School Paperwork CodeWars.js

---
+++ Beginner Series #1 School Paperwork CodeWars.js
@@ -0,0 +1,18 @@
+/*
+Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+Your task is to calculate how many blank pages do you need.
+
+Example:
+paperwork(5, 5) == 25
+Note! if n or m < 0 return 0! Waiting for translations and Feedback! Thanks!
+
+*/
+function paperwork(n, m) {
+  return n < 0 || m < 0 ? 0 : n * m;
+}
+
+console.log(paperwork(5, 12));
+console.log(paperwork(5, -2));
+console.log(paperwork(5, 2));
+console.log(paperwork(-5, 12));
